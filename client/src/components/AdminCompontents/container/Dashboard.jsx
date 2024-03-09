757b1fd5581776dc09c5a81697dd0559f1e1e148
abhinv <abhinavroy2099@gmail.com>
2024-03-09 14:14:06 +0530

    update backend room and homecontainer

--- client/src/components/AdminCompontents/container/Dashboard.jsx
+++ client/src/components/AdminCompontents/container/Dashboard.jsx
@@ -6,8 +6,8 @@ function AdminDashboard({ children }) {
   const navigate = useNavigate();
   const role = useSelector((state) => state.adminauth.role);
   return (
-    <div className="w-full max-w-6xl mx-auto px-4 flex justify-center items-center h-screen overflow-hidden">
-      <div className=" w-1/4  border-red-500 border-2 h-4/5 p-2 flex flex-col">
+    <div className="w-full max-w-7xl mx-auto px-4 flex justify-center items-center h-screen overflow-hidden">
+      <div className=" w-1/6  border-red-500 border-2 h-4/5 p-2 flex flex-col">
         <button
           onClick={() => {
             // navigate(`/myaccount/${userID}`);
@@ -32,24 +32,24 @@ function AdminDashboard({ children }) {
         >
           Rooms
         </button>
-        <button
+        {/* <button
           onClick={() => {
             // navigate(`/user/job/${userID}`);
           }}
           className="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
         >
           Jobs
-        </button>
+        </button> */}
         <button
           onClick={() => {
-            // navigate(`/user/bussiness/${userID}`);
+            navigate(`/admin/allbussiness`);
           }}
           className="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
         >
           bussiness
         </button>
       </div>
-      <main className=" w-3/4 border-red-500 border-2 h-4/5 p-2 ">
+      <main className=" w-10/12 border-red-500 border-2 h-4/5 p-2 ">
         {children}
       </main>
     </div>
