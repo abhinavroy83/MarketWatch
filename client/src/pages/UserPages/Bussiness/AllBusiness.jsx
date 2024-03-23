eb5f62405ac8ef5750448066d0ff678660b4c435
Aashna <aashnasagar19@gmail.com>
2024-03-23 11:18:06 +0530

    fix

--- client/src/pages/UserPages/Bussiness/AllBusiness.jsx
+++ client/src/pages/UserPages/Bussiness/AllBusiness.jsx
@@ -8,7 +8,7 @@ function AllBusiness() {
   const currentloc = useSelector((state) => state.auth.location);
   const [deatails, setdetails] = useState([]);
   const usercity = useSelector((state) => state.auth.city);
-
+  //fix
   const fetchallbusiness = async () => {
     try {
       const res = await axios.get(
@@ -34,7 +34,7 @@ function AllBusiness() {
   };
   return (
     <ChildContainer className="mt-30 w-full p-8 max-w-[1600px] m-auto">
-      <LeafletMap style={{ height: "300px", width: "100%"}} />
+      <LeafletMap style={{ height: "300px", width: "100%" }} />
       <div className="px-2 py-2 md:px md:py-10 font-[opensans] w-full max-w-[1600px] m-auto">
         <h1 className=" text-black text-4xl font-bold lg:text-4xl">
           {usercity ? (
@@ -44,9 +44,11 @@ function AllBusiness() {
           )}
         </h1>
         <h1 className=" text-black text-xl mt-3 font-bold lg:text-2xl">
-         265 Jobs are here</h1>
-         <h1 className=" text-gray-500 mt-1 text-md font-bold lg:text-1xl">
-         Jobs displayed are -</h1>
+          265 Jobs are here
+        </h1>
+        <h1 className=" text-gray-500 mt-1 text-md font-bold lg:text-1xl">
+          Jobs displayed are -
+        </h1>
         <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:mt-3 xl:grid-cols-2 xl:gap-4">
           {renderRooms()}
         </div>
