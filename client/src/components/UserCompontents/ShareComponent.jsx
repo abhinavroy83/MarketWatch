8bddc1b45e6ac43fadba733afa5d7ddf26943b31
aashna1504 <112244344+aashna1504@users.noreply.github.com>
2024-06-04 23:31:10 +0530

    new final chnages

--- client/src/components/UserCompontents/ShareComponent.jsx
+++ client/src/components/UserCompontents/ShareComponent.jsx
@@ -3,6 +3,8 @@ import { FaWhatsapp } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { TbBrandTwitter } from "react-icons/tb";
 import { IoCloseCircleSharp } from "react-icons/io5";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { FaShareAlt } from "react-icons/fa";
 
 import {
   FacebookShareButton,
@@ -10,8 +12,12 @@ import {
   WhatsappShareButton,
 } from "react-share";
 const ShareComponent = ({ url, title, onClose }) => {
+  const handlecopy = () => {
+    alert("Link Copied");
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-12 max-w-2xl relative p-20">
+    <div className="grid grid-cols-4 gap-12 max-w-2xl relative p-20">
       <div className="text-green-600 text-[25px] font-bold items-center justify-center w-full hover:text-black">
         <WhatsappShareButton
           className="flex flex-col gap-2 items-center justify-center"
@@ -44,6 +50,15 @@ const ShareComponent = ({ url, title, onClose }) => {
           <p className=" text-2xl">Twitter</p>
         </TwitterShareButton>
       </div>
+
+      <div className="text-gray-600 text-[25px] font-bold items-center hover:text-black text">
+                <CopyToClipboard 
+                     className="flex flex-col gap-2 items-center justify-center ml-2"
+                     text={url} onCopy={handlecopy}>
+                <FaShareAlt size={27} />
+                </CopyToClipboard>
+                <p className=" text-2xl mt-3">Link</p>
+       </div>
       <div className=" absolute right-1 top-1">
         <button onClick={onClose}>
           <IoCloseCircleSharp
