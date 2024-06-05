62df41285e3b5737bc80de6c12a2b95fe9367603
Abhinav <abhinavroy2099@gmail.com>
2024-06-05 12:36:13 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -18,6 +18,8 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5173",
       "https://main.d1iwqvmzn1plk4.amplifyapp.com",
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5174",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
