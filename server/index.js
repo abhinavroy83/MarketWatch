4a430c5d9a35e456127365c6743083e45cc5a18c
Abhinav <abhinavroy2099@gmail.com>
2024-06-10 09:03:54 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(
       "https://market-watch-woad.vercel.app",
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://main.d1iwqvmzn1plk4.amplifyapp.com",
+      "http://verydesi.com",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174",
       "https://copy-market-watch.vercel.app",
