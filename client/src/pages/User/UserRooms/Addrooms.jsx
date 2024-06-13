9480eb8793481cc2a4d79e11ffbb5aee717327d0
Abhinav <abhinavroy2099@gmail.com>
2024-06-13 11:46:38 +0530

    minor_changes_in_addrooom

--- client/src/pages/User/UserRooms/Addrooms.jsx
+++ client/src/pages/User/UserRooms/Addrooms.jsx
@@ -642,179 +642,6 @@ function Addrooms() {
             </div>
           </div>
 
-          <div className=" shadow-gray-300 w-[1300px] items-center justify-center p-4 mt-4">
-            {/* <p className="text-2xl   font-semibold text-black flex items-center justify-center gap-2 p-1">
-              <IoInformationCircleSharp />
-              Property Details-
-            </p> */}
-            <article className="flex justify-between mb-3 mt-5 px-4">
-              <div className=" flex items-center">
-                <div className="flex items-center">
-                  <label
-                    className="min-w-[120px] text-[21px] items-center "
-                    htmlFor=""
-                  >
-                    Rent
-                  </label>
-                  <article className="relative">
-                    <span className="absolute top-2 left-0 flex items-center pl-1 text-gray-500">
-                      $
-                    </span>
-                    <input
-                      id="rent"
-                      name="rent"
-                      type="number"
-                      placeholder=""
-                      {...register("rent", {
-                        required: "Rent required",
-                      })}
-                      className="flex h-10 font-['udemy-regular'] w-[400px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                    />
-                  </article>
-                </div>
-                <div className="flex justify-between ml-[6.5rem]">
-                  {!uti && (
-                    <div className="flex items-center">
-                      <label className="min-w-[120px] text-[21px]">
-                        Utilities
-                      </label>
-                      <article className="relative">
-                        <span className="absolute top-2 left-0 flex items-center pl-1 text-gray-500">
-                          $
-                        </span>
-                        <input
-                          type="text"
-                          {...register("utilities", {
-                            required: "utilities required",
-                          })}
-                          className="flex h-10 font-['udemy-regular'] w-[300px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                        />
-                      </article>
-                    </div>
-                  )}
-                  <div className="flex gap-3 items-center ml-7">
-                    <input
-                      className=""
-                      type="checkbox"
-                      checked={uti}
-                      onChange={(e) => setuti(e.target.checked)}
-                    />
-                    <p className="flex flex-col">Includes utility</p>
-                    {errors.utilities && (
-                      <p className="text-[16px] mt-1 text-red-500">
-                        {errors.utilities.message}
-                      </p>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </article>
-            <article className="flex px-4">
-              <div>
-                <div className="flex items-center">
-                  <label className="min-w-[120px] text-[21px]" htmlFor="">
-                    Bed
-                  </label>
-                  <div>
-                    <select
-                      className="flex h-10 font-['udemy-regular'] w-[400px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                      {...register("bed", {
-                        required: "Bed is required",
-                      })}
-                      defaultValue=""
-                    >
-                      <option value="" disabled hidden>
-                        Select Bed
-                      </option>
-                      <option className="text-[16px]" value="1">
-                        1
-                      </option>
-                      <option className="text-[16px]" value="2">
-                        2
-                      </option>
-                      <option className="text-[16px]" value="3">
-                        3
-                      </option>
-                      <option className="text-[16px]" value="4">
-                        4
-                      </option>
-                      <option className="text-[16px]" value="5">
-                        5
-                      </option>
-                      <option className="text-[16px]" value="6">
-                        6
-                      </option>
-                    </select>
-                    <p className="text-[16px] mt-1 text-red-500">
-                      {" "}
-                      {errors.bed && <p>{errors.bed.message}</p>}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-between ml-[6.5rem]">
-                <label className="min-w-[120px] text-[21px]" htmlFor="">
-                  Bath
-                </label>
-                <div>
-                  <select
-                    className="flex h-10 font-['udemy-regular'] w-[300px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                    {...register("bath", {
-                      required: "Bath is required",
-                    })}
-                    defaultValue=""
-                  >
-                    <option value="" disabled hidden>
-                      Select Bath
-                    </option>
-                    <option className="text-[16px]" value="separate">
-                      Separate
-                    </option>
-                    <option className="text-[16px]" value="shared">
-                      Shared
-                    </option>
-                  </select>
-                  <p className="text-[16px] mt-1 text-red-500">
-                    {" "}
-                    {errors.bath && <p>{errors.bath.message}</p>}
-                  </p>
-                </div>
-              </div>
-            </article>
-            <div>
-              <div className="flex items-center mt-2 px-4">
-                <label className="min-w-[120px] text-[21px]" htmlFor="">
-                  Laundary
-                </label>
-                <div>
-                  <select
-                    className="flex h-10 font-['udemy-regular'] w-[400px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                    {...register("laundary", {
-                      required: "laundary is required",
-                    })}
-                    defaultValue=""
-                  >
-                    <option value="" disabled hidden>
-                      Select laundary
-                    </option>
-                    <option className="text-[16px]" value="Available">
-                      Available
-                    </option>
-                    <option className="text-[16px]" value="Availableinapt">
-                      Available in apartment
-                    </option>
-                    <option className="text-[16px]" value="notAvailable">
-                      notAvailable
-                    </option>
-                  </select>
-                  <p className="text-[16px] text-red-500 mt-1">
-                    {" "}
-                    {errors.laundary && <p>{errors.laundary?.message}</p>}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
           {/* Area Name- */}
           <div className=" shadow-gray-300 w-[1300px] items-center justify-center p-4 mt-4">
             {/* <p className="text-2xl text-black font-semibold flex items-center justify-center gap-2 p-1">
@@ -822,6 +649,16 @@ function Addrooms() {
               Area Name
             </p> */}
             <article className="flex flex-col gap-4 px-4 mt-4">
+              <FormInput
+                className=""
+                label="Address"
+                type="text"
+                placeholder="Enter Address"
+                {...register("address", {
+                  required: "Address required",
+                })}
+                errorMessage={errors.address?.message}
+              />
               <div className="flex">
                 <div>
                   <div className="flex items-center">
@@ -893,7 +730,7 @@ function Addrooms() {
                   </div>
                 </div>
               </div>
-
+              {/* Subarea */}
               <div className="flex ">
                 <div className="flex items-center">
                   <label className="min-w-[120px] text-[21px]" htmlFor="">
@@ -951,16 +788,6 @@ function Addrooms() {
                   </div>
                 </div>
               </div>
-              <FormInput
-                className=""
-                label="Address"
-                type="text"
-                placeholder="Enter Address"
-                {...register("address", {
-                  required: "Address required",
-                })}
-                errorMessage={errors.address?.message}
-              />
 
               <FormInput
                 label="Posted By"
@@ -973,124 +800,7 @@ function Addrooms() {
               />
             </article>
           </div>
-          <div className="font-['udemy-regular']  shadow-gray-300 w-[1300px] p-4 mt-4">
-            {/* <p className="text-2xl   font-semibold text-black flex items-center justify-center gap-2 p-1">
-              <IoInformationCircleSharp />
-              Personal Details-
-            </p> */}
-            <article className="flex flex-col gap-4 px-4 mt-5">
-              <div className="flex w-auto">
-                <FormInput
-                  label="Email"
-                  type="text"
-                  placeholder="Enter Email"
-                  {...register("email", {
-                    required: "Email required",
-                  })}
-                  errorMessage={errors.email?.message}
-                />
-                <FormInput
-                  label="Number"
-                  type="text"
-                  placeholder="Enter Number"
-                  {...register("number", {
-                    required: "Number required",
-                  })}
-                  errorMessage={errors.number?.message}
-                />
-              </div>
-              <div>
-                <div className="flex items-center">
-                  <label className="min-w-[120px] text-[21px]" htmlFor="">
-                    Gender
-                  </label>
-                  <div className="">
-                    <select
-                      className="flex h-10 font-['udemy-regular'] w-[400px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-400 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                      {...register("gender", {
-                        required: "Gender is required",
-                      })}
-                      defaultValue=""
-                    >
-                      <option value="" disabled hidden>
-                        Select Gender
-                      </option>
-                      <option className="text-[16px]" value="male">
-                        Male
-                      </option>
-                      <option className="text-[16px]" value="female">
-                        Female
-                      </option>
-                      <option className="text-[16px]" value="both">
-                        Both
-                      </option>
-                    </select>
-                    <p className="text-[16px] mt-1 text-red-500">
-                      {" "}
-                      {errors.gender && <p>{errors.gender.message}</p>}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </article>
-            <label className="w-[290px] text-[21px] px-4">Image:</label>
-            <input
-              className="ml-9 py-4 "
-              type="file"
-              accept="image/*"
-              onChange={handleimgchange}
-            />
-          </div>
-          <div className=" shadow-gray-300 w-[1300px] items-center justify-center p-4 pb-0">
-            {/* <p className="text-2xl  font-semibold text-black flex items-center justify-center gap-2 p-1">
-              <IoInformationCircleSharp />
-              Add Description-
-            </p> */}
-            <article className="gap-4 items-center mt-5 px-4">
-              {/* <FormInput
-                className=""
-                label="Your Title"
-                placeholder="Title"
-                type="text"
-                {...register("Title", { required: "Title is required" })}
-                // errorMessage={errors.AdName?.message}
-              /> */}
-              {/* <div className="">
-                <label
-                  className="text-[21px] w-[120px] font-['udemy-regular'] leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 inline-block"
-                  htmlFor=""
-                >
-                  Title
-                </label>
-                <input
-                  className="font-['udemy-regular'] h-10  w-[400px] text-[21px] rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  label="Title"
-                  type="text"
-                  placeholder="Title"
-                  {...register("description", {
-                    required: "Title is required",
-                  })}
-                  // errorMessage={errors.area?.message}
-                />
-              </div> */}
-              {/* <div className="mt-5 flex items-center">
-                <label
-                  className="text-[21px] w-[120px] items-center justify-center font-['udemy-regular'] peer-disabled:cursor-not-allowed peer-disabled:opacity-70 inline-block"
-                  htmlFor=""
-                >
-                  Description
-                </label>
-                <textarea
-                  className="h-100px w-[400px] text-[21px] font-['udemy-regular'] items-center justify-center text-21px rounded-md border border-black/30 bg-transparent px-3 py-2 placeholder:text-gray-600 bg-white focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  name="description"
-                  placeholder="Description"
-                  {...register("description", {
-                    required: "Description is required",
-                  })}
-                />
-              </div> */}
-            </article>
-          </div>
+
           <button
             type="submit"
             className="rounded-md bg-green-800 my-5 mt-0 px-4 py-2 text-[18px] self-center font-semibold text-white shadow-sm hover:bg-green-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
