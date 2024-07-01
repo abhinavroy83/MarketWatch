fdadb998bc660fe34d396188d6b05c7e36a31643
Abhinav <abhinavroy2099@gmail.com>
2024-07-01 18:28:24 +0530

    update_url

--- client/src/pages/AdminPage/Area/AddArea.jsx
+++ client/src/pages/AdminPage/Area/AddArea.jsx
@@ -32,7 +32,7 @@ function AddArea({ editdata }) {
     if (editdata) {
       try {
         const res = await axios.put(
-          `http://localhost:8000/api/admin/updatearea/${editdata?._id}`,
+          `https://api.verydesi.com/api/admin/updatearea/${editdata?._id}`,
           data,
           {
             headers: {
@@ -55,7 +55,7 @@ function AddArea({ editdata }) {
     } else {
       try {
         const res = await axios.post(
-          `http://localhost:8000/api/admin/postcity`,
+          `https://api.verydesi.com/api/admin/postcity`,
           data,
           {
             headers: {
