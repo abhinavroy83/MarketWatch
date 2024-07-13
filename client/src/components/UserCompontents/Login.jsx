55881a149fc836371cf26d4ca33e202706dbdc74
Abhinav <abhinavroy2099@gmail.com>
2024-07-13 12:15:42 +0530

    update_login_Input

--- client/src/components/UserCompontents/Login.jsx
+++ client/src/components/UserCompontents/Login.jsx
@@ -198,7 +198,39 @@ function Login() {
               onSubmit={handleSubmit(onsubmit)}
               className="flex flex-col mt-2 gap-3 font-['udemy-regular']"
             >
-              <Input
+              <label
+                htmlFor="Email"
+                className={`relative block py-2 rounded-md border shadow-sm focus-within:ring-1 ${
+                  errors.email
+                    ? "border-red-600 focus-within:border-red-600 focus-within:ring-red-600"
+                    : "border-gray-200 focus-within:border-blue-600 focus-within:ring-blue-600"
+                }`}
+              >
+                <input
+                  type="text"
+                  id="Email"
+                  className="peer border-none px-2 bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: "Email is Required",
+                    validate: {
+                      matchPatern: (value) =>
+                        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+                          value
+                        ) || "Email address must be a valid address",
+                    },
+                  })}
+                />
+
+                <span
+                  className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs ${
+        errors.email ? 'text-red-600' : 'text-gray-700'
+      }"
+                >
+                  {errors.email ? errors.email.message : "Email"}
+                </span>
+              </label>
+              {/* <Input
                 label="Email"
                 Placeholder="Email"
                 type="Email"
@@ -213,7 +245,7 @@ function Login() {
                 })}
                 errorMessage={errors.email?.message}
                 className="!w-full"
-              />
+              /> */}
               <Input
                 label="Password"
                 Placeholder="Password"
