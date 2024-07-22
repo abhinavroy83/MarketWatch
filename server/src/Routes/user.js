17fc5fd5b621d867388953f9b716b2ec68cd7324
Abhinav <abhinavroy2099@gmail.com>
2024-07-22 23:21:19 +0530

    added_verifypassword

--- server/src/Routes/user.js
+++ server/src/Routes/user.js
@@ -6,16 +6,18 @@ const findspecifcuser = require("../Controllers/user/finduser");
 const updateuser = require("../Controllers/user/updateuser");
 const verifyemail = require("../Controllers/user/verifyuser");
 const deleteuser = require("../Controllers/user/Deleteuser");
+const verifypassword = require("../Controllers/user/verifypassword");
 const {
   forgetPassword,
   resetpassword,
-} = require("../Controllers/user/reserpassword");  
+} = require("../Controllers/user/reserpassword");
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get(`/dashboard/profile/:_id`, findspecifcuser);
 router.put(`/updateuser/:_id`, updateuser);
 router.get(`/verifyemail/:jwttoken`, verifyemail);
+router.post("/verifypassword", verifypassword);
 router.post("/forgotpassword", forgetPassword);
 router.post("/resetpassword/:token", resetpassword);
 router.delete("/deleteuser/:_id", deleteuser);
