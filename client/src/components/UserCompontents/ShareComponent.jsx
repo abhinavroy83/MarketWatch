7cd7a160bb793722112f707b3b540d9dcecb0621
Aashna Sagar <112244344+aashna1504@users.noreply.github.com>
2024-08-27 16:00:23 +0530

    links

--- client/src/components/UserCompontents/ShareComponent.jsx
+++ client/src/components/UserCompontents/ShareComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { FaWhatsapp, FaFacebook, FaShareAlt } from "react-icons/fa";
 import { TbBrandTwitter } from "react-icons/tb";
 import { IoCloseCircleSharp } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
 import CopyToClipboard from "react-copy-to-clipboard";
 import {
   FacebookShareButton,
@@ -46,57 +47,64 @@ const ShareComponent = ({ url, title, onClose }) => {
   }, [onClose]);
 
   return (
-    <div ref={ref} className="grid grid-cols-4 gap-12 max-w-2xl relative p-20">
-      <div className="text-green-600 text-[25px] font-bold items-center justify-center w-full hover:text-black">
+    <div
+      ref={ref}
+      className="lg:grid lg:grid-cols-4 items-center flex flex-col lg:gap-[7rem] gap-12 max-w-2xl relative lg:p-20 p-[4rem]"
+    >
+      <div className="text-green-600 text-[25px] items-center justify-center w-full hover:text-black">
         <WhatsappShareButton
           className="flex flex-col gap-2 items-center justify-center"
           url={url}
           title={title}
         >
           <FaWhatsapp size={32} className="items-center" />
-          <p className=" text-2xl">WhatsApp</p>
+          <p className="text-[25px]">WhatsApp</p>
         </WhatsappShareButton>
       </div>
 
-      <div className="text-blue-600 text-[25px] font-bold items-center hover:text-black">
+      <div className="text-blue-600 text-[25px] items-center hover:text-black">
         <FacebookShareButton
           className="flex flex-col gap-2 items-center justify-center"
           url={url}
           quote={title}
         >
           <FaFacebook size={32} />
-          <p className=" text-2xl">Facebook</p>
+          <p className="text-[25px]">Facebook</p>
         </FacebookShareButton>
       </div>
 
-      <div className="text-red-600 text-[25px] font-bold items-center hover:text-black">
+      <div className="text-red-600 text-[25px] items-center hover:text-black">
         <TwitterShareButton
           className="flex flex-col gap-2 items-center justify-center"
           url={url}
           title={title}
         >
           <TbBrandTwitter size={32} />
-          <p className=" text-2xl">Twitter</p>
+          <p className="text-[25px]">Twitter</p>
         </TwitterShareButton>
       </div>
 
-      <div className="text-gray-600 text-[25px] font-bold items-center hover:text-black text">
+      <div className="text-gray-600 text-[25px] items-center hover:text-black text">
         <CopyToClipboard
-          className="flex flex-col gap-2 items-center justify-center ml-2"
+          className="flex flex-col gap-2 items-center justify-center"
           text={url}
           onCopy={handleCopy}
         >
-          <FaShareAlt size={27} />
+          <FaShareAlt size={27} className="" />
         </CopyToClipboard>
-        <p className=" text-2xl mt-3">Link</p>
+        <p className="text-[25px] mt-3">Link</p>
       </div>
 
       <div className="absolute right-1 top-1">
         <button onClick={onClose}>
-          <IoCloseCircleSharp
+          <RxCross1
+            className="h-5 w-5 text-black absolute top-3 right-3 cursor-pointer hover:rotate-[360deg] transition-transform duration-300 "
+            onClick={() => handleModal(false, false)}
+          />
+          {/* <IoCloseCircleSharp
             size={35}
             className="font-bold hover:text-red-800"
-          />
+          /> */}
         </button>
       </div>
     </div>
